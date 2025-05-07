744822bb11a54ab1131833e449c6ddef61dc6897
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-06 19:42:15 -0500

    07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -4,7 +4,7 @@ var string = 'hello'
 
 
 const repeatString = function(string, times) {
-  if(times < 0) return ''
+  if(times < 0) return 'ERROR'
   let result = ''
   for(let i = 0; i < times; i++){ 
     result += string
