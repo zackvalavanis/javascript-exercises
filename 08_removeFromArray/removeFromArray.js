8ebd95c9c2b2beede36076530be284d77f862ec7
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-06 19:55:27 -0500

    08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -2,12 +2,17 @@ var array = [1,2,3,4,5]
 
 
 
-const removeFromArray = function(array) {
-  let result = new Array(...array)
-  return result
+const removeFromArray = function(array, ...args) {
+  const newArray = []
+  array.forEach((item) => { 
+    if(!args.includes(item)) { 
+      newArray.push(item)
+    }
+  })
+  return newArray
 };
 
-console.log(removeFromArray(array))
+console.log(removeFromArray(array, 3, 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
