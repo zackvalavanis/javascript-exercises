6b0e78963818a14a35019e337269e9425f48d329
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-06 19:11:38 -0500

    question 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+var times = 10;
+var string = 'hello'
 
+
+
+const repeatString = function(string, times) {
+  if(times < 0) return ''
+  let result = ''
+  for(let i = 0; i < times; i++){ 
+    result += string
+  }
+  return result
 };
+
+console.log(repeatString(string, times))
 
 // Do not edit below this line
 module.exports = repeatString;
