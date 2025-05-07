4d312107cb3ae34c63f94418658e5b7cbed84382
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-06 20:04:39 -0500

    09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+var number1 = 1;
+var number2 = 5
 
+
+const sumAll = function(number1, number2) {
+
+  if(typeof(number1) !== 'number' || typeof(number2) !== 'number' || number1 < 0 || number2 < 0 || !Number.isInteger(number1) || !Number.isInteger(number2)) { 
+    return 'ERROR'
+  }
+
+  var sumTotal = 0;
+
+  if(number1 > number2) { 
+    [number1, number2] = [number2, number1]
+  }
+
+  for(let i = number1; i <= number2; i++){ 
+    sumTotal += i
+  }
+  return sumTotal
 };
+
+console.log(sumAll(number1, number2))
 
 // Do not edit below this line
 module.exports = sumAll;
