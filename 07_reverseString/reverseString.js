b332b9abaa76c4381c5b4dcc762e6a934d193bc2
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-06 19:17:04 -0500

    07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  return string.split('').reverse().join('')
 };
+
+console.log(reverseString('olleh'))
 
 // Do not edit below this line
 module.exports = reverseString;
