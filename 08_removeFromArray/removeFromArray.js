5d3d6d68bfb8a24bb055220c73f048a9064d1602
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-06 19:32:32 -0500

    practice with testing and writing testable code

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+var array = [1,2,3,4,5]
+
+
+
+const removeFromArray = function(array) {
+  let result = new Array(...array)
+  return result
 };
+
+console.log(removeFromArray(array))
 
 // Do not edit below this line
 module.exports = removeFromArray;
