9136b6a79407b26a08ed22b064d0f5da5535fd8f
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-19 19:47:04 -0500

    isPalindrome with tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+string = '!tit!'
 
+const palindromes = function (string) {
+  let cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+  let stringy = cleanedString.split('').reverse().join('')
+  return cleanedString === stringy
 };
+
+console.log(palindromes(string))
 
 // Do not edit below this line
 module.exports = palindromes;
