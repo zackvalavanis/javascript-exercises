080c58c946e2b4a3bc2925335091f6953c2251bf
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-19 20:05:06 -0500

    finished with passed tests

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,54 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+
+
+const getTheTitles = function(books) {
+  let array = []
+  books.map((b) => { 
+    array.push(b.title)
+  })
+  return array
 };
+
+console.log(getTheTitles(books))
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+// # Exercise 15 - Get the Titles!
+
+// You are given an array of objects that represent books with an author and a title that looks like this:
+
+// ```javascript
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
+// ```
+
+// Your job is to write a function that takes the array and returns an array of titles:
+
+// ```javascript
+// getTheTitles(books) // ['Book','Book2']
+// ```
+
+// ## Hints
+
+// - You should use a built-in javascript method to do most of the work for you!
